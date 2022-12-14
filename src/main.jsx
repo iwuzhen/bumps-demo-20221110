aa5b9a673e30eacd16c2aa5f598646494e3d98fb
ider <326737833@qq.com>
2022-12-14 14:48:02 +0800

    future:new chart

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Root from "./routes/root";
 import Demo1 from "./routes/demo/demo_1";
 import Demo2 from "./routes/demo/demo_2";
 import Demo3 from "./routes/demo/demo_3";
+import Demo4 from "./routes/demo/demo_4";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
   {
     path: "/demo/demo-3",
     element: <Demo3 />,
+  },
+  {
+    path: "/demo/demo-4",
+    element: <Demo4 />,
   }
 ]);
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,6 +5,7 @@ export default function Root() {
           <button style={{marginRight:'20px'}}><a href="/demo/demo-1">整体按迭代次数的知识复杂度</a></button>
           <button style={{marginRight:'20px'}}><a href="/demo/demo-2">世界知识复杂度排名</a></button>
           <button style={{marginRight:'20px'}}><a href="/demo/demo-3">经济复杂度(ECI country ranking)</a></button>
+          <button style={{marginRight:'20px'}}><a href="/demo/demo-4">知识复杂度 - [2022-12-14]</a></button>
           {/* <button style={{marginRight:'20px'}}><a href="/demo/demo-3">世界知识复杂度排名+中美湾区</a></button> */}
       </>
     );
